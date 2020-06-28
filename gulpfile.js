3ec4b2ab2124167973a8ec81174fc90609e59fc5
vxhly <pengchengou@gmail.com>
2020-06-28 19:51:15 +0800

    修改依赖包

--- gulpfile.js
+++ gulpfile.js
@@ -34,20 +34,7 @@ gulp.task('build', function () {
     .pipe($.sass.sync()
       .on('error', $.sass.logError))
     .pipe($.autoprefixer({
-      browsers: [
-        'ie >= 8',
-        'ie_mob >= 10',
-        'ff >= 26',
-        'chrome >= 20',
-        'safari >= 6',
-        'opera >= 12',
-        'Firefox >= 24',
-        'Explorer >= 8',
-        'ios >= 5',
-        'android >= 2.3',
-        'bb >= 10',
-        'last 1 version'
-      ]
+      cascade: false
     }))
     .pipe($.csscomb())
     .pipe($.cssbeautify({
--- webpack.config.js
+++ webpack.config.js
@@ -33,22 +33,7 @@ module.exports = {
           loader: 'postcss-loader',
           options: {
             plugins: () => [
-              autoprefixer({
-                browsers: [
-                  'ie >= 8',
-                  'ie_mob >= 10',
-                  'ff >= 26',
-                  'chrome >= 20',
-                  'safari >= 6',
-                  'opera >= 12',
-                  'Firefox >= 24',
-                  'Explorer >= 8',
-                  'ios >= 5',
-                  'android >= 2.3',
-                  'bb >= 10',
-                  'last 1 version'
-                ]
-              })
+              autoprefixer()
             ]
           }
         },
